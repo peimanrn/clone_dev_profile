a27363598876dc3f112cf9a4fb1757c09c7b7b51
Peiman Ramezani Nejad <peiman.ramezaninejad@gmail.com>
2018-06-28 21:14:17 +0430

    Project Created.

---
+++ hss/hss/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  hss
+//
+//  Created by peiman on 6/28/18.
+//  Copyright © 2018 moradi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
